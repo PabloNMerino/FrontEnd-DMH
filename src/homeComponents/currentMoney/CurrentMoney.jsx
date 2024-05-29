1904b7c27f35786ce24a2f28d10223850765c6ec
Pablo Merino <pablomerino@MacBook-Air-de-Pablo.local>
2024-05-29 18:46:13 -0300

    creating links

--- src/homeComponents/currentMoney/CurrentMoney.jsx
+++ src/homeComponents/currentMoney/CurrentMoney.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Styles from './CurrentMoneyStyle.module.css'
 
 export const CurrentMoney = () => {
@@ -9,15 +10,21 @@ export const CurrentMoney = () => {
                 <p className={Styles.amount}>$32500</p>
             </div>
             <div className={Styles.buttons}>
-                <div className={Styles.btn}>
-                    <p>Ver CVU y Alias</p>
-                </div>
-                <div className={Styles.btn}>
-                    <p>Ingresar Dinero</p>
-                </div>
-                <div className={Styles.btn}>
-                    <p>Enviar Dinero</p>
-                </div>
+                <Link className={Styles.linkInfo}>
+                    <div className={Styles.btn}>
+                        <p>Ver CVU y Alias</p>
+                    </div>
+                </Link>
+                <Link className={Styles.linkInfo}>
+                    <div className={Styles.btn}>
+                        <p>Ingresar Dinero</p>
+                    </div>
+                </Link>
+                <Link className={Styles.linkInfo}>
+                    <div className={Styles.btn}>
+                        <p>Enviar Dinero</p>
+                    </div>
+                </Link>
             </div>
         </article>
     )
